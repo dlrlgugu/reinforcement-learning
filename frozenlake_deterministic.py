5e7222911398d4560833da4b19054b837fe7230d
dlrlgugu <hckguja@gmail.com>
2018-07-24 11:17:06 -0700

    deterministic Signed-off-by: dlrlgugu <hckguja@gmail.com>

---
+++ frozenlake_deterministic.py
@@ -0,0 +1,76 @@
+import gym
+from gym.envs.registration import register
+
+import readchar
+
+class _Getch:
+    def __call__(self):
+        fd=sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch=sys.stdin.read(3)
+        finally:
+            termios.tcsetattr(fd,termios.TCSADRAIN,old_settings)
+        return ch
+
+inkey = _Getch()
+
+LEFT=0
+DOWN=1
+RIGHT=2
+UP=3
+
+arrow_keys={
+    '\x1b[A':UP,
+    '\x1b[B':DOWN,
+    '\x1b[C':RIGHT,
+    'x1b[0':LEFT
+    }
+
+
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name':'4x4','is_slippery':False}
+    )
+
+env=gym.make('FrozenLake-v3')
+env.render()
+
+while True:
+    key=readchar.readkey()
+    if key not in arrow_keys.keys():
+        print("nah")
+        break
+
+    action = arrow_keys[key]
+    state , reward , done , info = env.step(action)
+    env.render()#render after action.
+    print("state: ",state,"Action: ",action,"Reward: ",reward,
+            "Info: ",info)
+
+    if done:
+        print("done. reward is : ",reward)
+        break
+    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
